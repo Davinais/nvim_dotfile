e166495c91d5ff9c23587f9a25ac2959f4ac68de
Davinais <david870620@gmail.com>
2023-05-20 04:07:59 +0800

    Feat: Add neovide configurations

--- init.lua
+++ init.lua
@@ -4,5 +4,10 @@ require('keymappings')
 require('plugins')
 require('lsp')
 
+-- Neovide configuration if this is Neovide
+if vim.g.neovide then
+    require('neovide')
+end
+
 vim.cmd[[colorscheme tokyonight]]
 
---
+++ lua/neovide.lua
@@ -0,0 +1,4 @@
+-- Display
+vim.o.guifont = "Fira Code:h14"
+vim.g.neovide_transparency = 0.92
+
