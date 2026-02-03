141d3f6419d3c4728aa8a5262c4a800a112f7b7a
Davinais <david870620@gmail.com>
2026-02-04 02:16:29 +0800

    Feat: Update lazy-load settings for cmp and LspSaga

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -128,11 +128,16 @@ local plugins = {
         event = "LspAttach",
         dependencies = {
             {"nvim-tree/nvim-web-devicons"},
+        },
+        opts = {
+            lightbuln = {
+                sign = false,
+            },
         }
     },
     {
         "hrsh7th/nvim-cmp",
-        event = "InsertEnter",
+        event = { "InsertEnter", "CmdlineEnter" },
         dependencies = {
             { "hrsh7th/cmp-nvim-lsp" },
             { "hrsh7th/cmp-buffer" },
@@ -140,7 +145,58 @@ local plugins = {
             { "hrsh7th/cmp-cmdline" },
             { "hrsh7th/cmp-vsnip" },
             { "hrsh7th/vim-vsnip" },
-        }
+        },
+        opts = function()
+            local lspkind = require("lspkind")
+            local cmp = require("cmp")
+            cmp.setup {
+                snippet = {
+                    expand = function(args)
+                        vim.fn["vsnip#anonymous"](args.body)
+                    end,
+                },
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "vsnip" }
+                }, {
+                    { name = "buffer" },
+                    { name = "path" }
+                }),
+                mapping = require("keymappings").cmp_keymaps(cmp),
+                formatting = {
+                format = lspkind.cmp_format({
+                    mode = 'symbol', -- show only symbol annotations
+                    maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                    ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+
+                    -- The function below will be called before any actual modifications from lspkind
+                    -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+                    before = function (entry, vim_item)
+                        -- Display source name
+                        vim_item.menu = "["..string.upper(entry.source.name).."]"
+                        return vim_item
+                    end
+                })
+                }
+            }
+            -- Use buffer source for `/`.
+            cmp.setup.cmdline('/', {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                    { name = 'buffer' }
+                }
+            })
+            -- Use cmdline & path source for ':'.
+            cmp.setup.cmdline(':', {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = 'path' }
+                }, {
+                    { name = 'cmdline' }
+                })
+            })
+            return {}
+        end
     },
     { "onsails/lspkind-nvim" }
 }
